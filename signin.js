13f1567fd1b7b5c3a9e1062222653c523aa970c7
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-30 00:49:21 +0500

    modified

--- signin.js
+++ signin.js
@@ -4,51 +4,64 @@ let loginEmail = document.getElementById("loginEmail");
 let loginBtn = document.getElementById("loginBtn")
 const auth = getAuth();
 loginBtn.addEventListener('click', () => {
-  if (loginEmail.value === "" && loginPassward.value === "") {
-    Swal.fire({
-      position: "top-end",
-      icon: "error",
-      title: "Please enter both email and password",
-      showConfirmButton: false,
-      timer: 1500
-    });
-  } else if (loginEmail.value === "") {
-    Swal.fire({
-      position: "top-end",
-      icon: "error",
-      title: "Please enter email",
-      showConfirmButton: false,
-      timer: 1500
-    });
-  } else if (loginPassward.value === "") {
-    Swal.fire({
-      position: "top-end",
-      icon: "error",
-      title: "Please enter password",
-      showConfirmButton: false,
-      timer: 1500
-    });
-  } else {
-    signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
-      .then((userCredential) => {
+  signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
+  .then((userCredential) => {
 
-        const user = userCredential.user;
-        console.log(user)
+    const user = userCredential.user;
+    console.log(user)
 
 
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage)
-        console.log(errorCode)
-      });
-    location = "./dashBoard/index.html"
-  }
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorMessage)
+    console.log(errorCode)
+  });
+location = "./dashBoard/index.html"
+  // if (loginEmail.value === "" && loginPassward.value === "") {
+  //   Swal.fire({
+  //     position: "top-end",
+  //     icon: "error",
+  //     title: "Please enter both email and password",
+  //     showConfirmButton: false,
+  //     timer: 1500
+  //   });
+  // } else if (loginEmail.value === "") {
+  //   Swal.fire({
+  //     position: "top-end",
+  //     icon: "error",
+  //     title: "Please enter email",
+  //     showConfirmButton: false,
+  //     timer: 1500
+  //   });
+  // } else if (loginPassward.value === "") {
+  //   Swal.fire({
+  //     position: "top-end",
+  //     icon: "error",
+  //     title: "Please enter password",
+  //     showConfirmButton: false,
+  //     timer: 1500
+  //   });
+  // } else {
+  //   signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
+  //     .then((userCredential) => {
+
+  //       const user = userCredential.user;
+  //       console.log(user)
+
+
+  //     })
+  //     .catch((error) => {
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       console.log(errorMessage)
+  //       console.log(errorCode)
+  //     });
+  //   location = "./dashBoard/index.html"
+  // }
 })
-// if (loginPassward.value.trim(), loginEmail.value.trim()) {
 
-// }
 
 
 
@@ -57,13 +70,24 @@ loginBtn.addEventListener('click', () => {
 
 //login form functionality
 const wrapper = document.querySelector(".wrapper"),
-  signupHeader = document.querySelector(".signup header"),
-  loginHeader = document.querySelector(".login header");
-loginHeader.addEventListener("click", () => {
-  wrapper.classList.add("active");
-});
+signupHeader = document.querySelector(".signup header")
+// loginHeader = document.querySelector(".login header");
+
+// loginHeader.addEventListener("click", () => {
+// wrapper.classList.add("active");
+// });
 signupHeader.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
+
+// const wrapper = document.querySelector(".wrapper"),
+//   signupHeader = document.querySelector(".signup header"),
+//   loginHeader = document.querySelector(".login header");
+// loginHeader.addEventListener("click", () => {
+//   wrapper.classList.add("active");
+// });
+// signupHeader.addEventListener("click", () => {
+//   wrapper.classList.remove("active");
+// });
 //login form functionality end
 
