2f2cd48de383fd15b9e3e70e2a90123c8181b993
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-30 00:01:59 +0500

    modified

--- app.js
+++ app.js
@@ -4,23 +4,66 @@ const auth = getAuth();
 let signupBtn = document.getElementById("signup");
 let signUpPassward= document.getElementById("signUpPassward");
 let signUpEmail=document.getElementById("signUpEmail");
-signupBtn.addEventListener('click',()=>{
-if(signUpEmail.value&&signUpPassward.value){
-createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPassward.value)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log("ERROR:"+errorMessage)
-    console.log("ERROR:"+errorCode)
-  });
-}else{
-  console.log("enter your data")
-}
-location="signin.html"
+signupBtn.addEventListener('click', () => {
+  if (signUpEmail.value === "" && signUpPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter both email and password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (signUpEmail.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter email",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (signUpPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else {
+    signInWithEmailAndPassword(auth, signUpPassward.value, signUpEmail.value)
+      .then((userCredential) => {
+
+        const user = userCredential.user;
+        console.log(user)
+
+
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        console.log(errorCode)
+      });
+   location="signin.html"
+  }
 })
+// signupBtn.addEventListener('click',()=>{
+// if(signUpEmail.value.trim&&signUpPassward.value.trim){
+// createUserWithEmailAndPassword(auth, signUpEmail.value, signUpPassward.value)
+//   .then((userCredential) => {
+//     const user = userCredential.user;
+//     console.log(user)
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     console.log("ERROR:"+errorMessage)
+//     console.log("ERROR:"+errorCode)
+//   });
+// }else{
+//   console.log("enter your data")
+// }
+// location="signin.html"
+// })
    
     
--- signin.js
+++ signin.js
@@ -1,24 +1,69 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-
+import { getAuth, signInWithEmailAndPassword } from "./firebase.js";
+let loginPassward = document.getElementById("loginPassward");
+let loginEmail = document.getElementById("loginEmail");
+let loginBtn = document.getElementById("loginBtn")
 const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+loginBtn.addEventListener('click', () => {
+  if (loginEmail.value === "" && loginPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter both email and password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (loginEmail.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter email",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (loginPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else {
+    signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
+      .then((userCredential) => {
+
+        const user = userCredential.user;
+        console.log(user)
 
 
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        console.log(errorCode)
+      });
+    location = "./dashBoard/index.html"
+  }
+})
+// if (loginPassward.value.trim(), loginEmail.value.trim()) {
+
+// }
+
+
+
+
+
+
+//login form functionality
 const wrapper = document.querySelector(".wrapper"),
-    signupHeader = document.querySelector(".signup header"),
-    loginHeader = document.querySelector(".login header");
+  signupHeader = document.querySelector(".signup header"),
+  loginHeader = document.querySelector(".login header");
 loginHeader.addEventListener("click", () => {
-    wrapper.classList.add("active");
+  wrapper.classList.add("active");
 });
 signupHeader.addEventListener("click", () => {
-    wrapper.classList.remove("active");
+  wrapper.classList.remove("active");
 });
+//login form functionality end
+
