71c3c736004b288490bd6bd49b33ce312687644a
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-10-31 10:18:06 +0500

    some functionalities added

--- signin.js
+++ signin.js
@@ -8,54 +8,54 @@ let loginEmail = document.getElementById("loginEmail");
 let loginBtn = document.getElementById("loginBtn")
 const auth = getAuth();
 loginBtn.addEventListener('click', () => {
-  signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
-  .then((userCredential) => {
-    const user = userCredential.user;
-    console.log(user)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-    console.log(errorCode)
-  });
-  // if (loginEmail.value === "" && loginPassward.value === "") {
-  //   Swal.fire({
-  //     position: "top-end",
-  //     icon: "error",
-  //     title: "Please enter both email and password",
-  //     showConfirmButton: false,
-  //     timer: 1500
-  //   });
-  // } else if (loginEmail.value === "") {
-  //   Swal.fire({
-  //     position: "top-end",
-  //     icon: "error",
-  //     title: "Please enter email",
-  //     showConfirmButton: false,
-  //     timer: 1500
-  //   });
-  // } else if (loginPassward.value === "") {
-  //   Swal.fire({
-  //     position: "top-end",
-  //     icon: "error",
-  //     title: "Please enter password",
-  //     showConfirmButton: false,
-  //     timer: 1500
-  //   });
-  // } else {
-  //   signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
-  //     .then((userCredential) => {
-  //       const user = userCredential.user;
-  //       console.log(user)
-  //     })
-  //     .catch((error) => {
-  //       const errorCode = error.code;
-  //       const errorMessage = error.message;
-  //       console.log(errorMessage)
-  //       console.log(errorCode)
-  //     });
-  // }
+  // signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
+  // .then((userCredential) => {
+  //   const user = userCredential.user;
+  //   console.log(user)
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   console.log(errorMessage)
+  //   console.log(errorCode)
+  // });
+  if (loginEmail.value === "" && loginPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter both email and password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (loginEmail.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter email",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else if (loginPassward.value === "") {
+    Swal.fire({
+      position: "top-end",
+      icon: "error",
+      title: "Please enter password",
+      showConfirmButton: false,
+      timer: 1500
+    });
+  } else {
+    signInWithEmailAndPassword(auth, loginPassward.value, loginEmail.value)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        console.log(errorCode)
+      });
+  }
   location = "/dashBoard/index.html"
 })
 
